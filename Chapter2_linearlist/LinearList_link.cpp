3bd72d674567088e7ac2a1b1de1a931bc3e22bdf
Feng-Jay <2652925120@qq.com>
2021-05-20 21:10:32 +0800

    Start LinkList

--- Chapter2_linearlist/LinearList_link.cpp
+++ Chapter2_linearlist/LinearList_link.cpp
@@ -8,7 +8,8 @@ typedef struct LNode{
     eleytpe data;
     struct LNode* next;//结构体自引用只能引用指针，若引用结构体，就会套娃，内存会用光。
 }LNode, *LinkList; 
-//
+//因为结构体内要有自引用指针，所以在C中必须要先给结构体命名
+//后面的LinkList<==>LNode *;是结构体的指针
 //为了兼容C，采用这种方式写
 
 
