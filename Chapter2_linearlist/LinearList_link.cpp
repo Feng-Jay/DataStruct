294db6c8f05636498a2342a6445069577af60d86
Feng-Jay <2652925120@qq.com>
2021-05-20 21:08:07 +0800

    Start LinkList

--- Chapter2_linearlist/LinearList_link.cpp
+++ Chapter2_linearlist/LinearList_link.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+typedef int eleytpe;
+
+typedef struct LNode{
+    eleytpe data;
+    struct LNode* next;//结构体自引用只能引用指针，若引用结构体，就会套娃，内存会用光。
+}LNode, *LinkList; 
+//
+//为了兼容C，采用这种方式写
+
+
+/*C++ Mode*/
+// struct LNode{
+//     eleytpe data;
+//     LNode *next;
+// };
+
+
 int main()
 {
     return 0;
