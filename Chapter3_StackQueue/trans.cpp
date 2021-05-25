f4ad2990ad42bace507621373bf4cd0e0b442905
Feng-Jay <2652925120@qq.com>
2021-05-25 21:51:35 +0800

    Chapter3

---
+++ Chapter3_StackQueue/trans.cpp
@@ -0,0 +1,27 @@
+/*学习C++的stack库的使用！*/
+/*本节进行数制转换！*/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int trans(stack<int> &mystack,int n,int tar)
+{
+    /*将十进制数n转换为tar进制数字*/
+    while(n){
+        mystack.push(n%tar);
+        n=n/tar;
+    }
+    while(!mystack.empty()){
+        int e=mystack.top();
+        mystack.pop();
+        cout<<e;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    stack<int> mystack;
+    trans(mystack,4,2);
+    return 0;
+}
