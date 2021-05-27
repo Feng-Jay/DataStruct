fc192a1663e1c41131d34453b6adb9f14b52c01e
Feng-Jay <2652925120@qq.com>
2021-05-27 08:24:46 +0800

    day10

---
+++ Chapter3_StackQueue/bracket_detect.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stack>
+#include <string.h>
+using namespace std;
+
+int test(stack<char> &mystack)
+{
+    char mylist[255];
+    int flag=0;
+    cout<<"Please input a string."<<endl;
+    cin>>mylist;
+    int len=strlen(mylist);
+    for(int i=0;i<len;i++){
+        if(mylist[i]=='['||mylist[i]=='('||mylist[i]=='{'){
+            mystack.push(mylist[i]);
+        }else{
+            if(mystack.empty()){
+                flag=1;
+                break;
+            }
+            switch (mylist[i])
+            {
+            case ']':
+            {
+                if(mystack.top()=='[')
+                mystack.pop();
+                else flag=1;
+                break;
+            }
+            case ')':
+            {
+                if(mystack.top()=='(')
+                mystack.pop();
+                else flag=1;
+                break;
+            }
+            case '}':
+            {
+                if(mystack.top()=='{')
+                mystack.pop();
+                else flag=1;
+                break;
+            }
+            default:
+            {
+                flag=1;
+                break;
+            }
+            }
+        }
+    }
+    if(flag){
+        cout<<"Incorrect format! Please test:";
+        cout<<mylist<<endl;
+    }else{
+        cout<<"Correct!"<<endl;
+    }
+    return 1;
+}
+
+int main()
+{
+    stack<char> mystack;
+    test(mystack);
+    return 0;
+}
