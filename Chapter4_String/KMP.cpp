ac7acbb098b6e2e755a9f200abff1119c7a88e2b
Feng-Jay <2652925120@qq.com>
2021-08-18 07:46:30 +0800

    fix get_next

--- Chapter4_String/KMP.cpp
+++ Chapter4_String/KMP.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 void get_next(string sample, int * next)
 {
-    next[0]=0;
+    next[0]=-1;
     int i,j;
-    i=0;
-    j=next[0];//用i作next数组索引，j表示sample中目前已知next值的最靠后下标
-    while (i<sample.length()-1){
-        if(j==0){
+    i=0; j=-1;//用i作next数组索引，j表示sample中目前已知next值的最靠后下标
+    while (i<sample.length()){
+        if(j==-1){
             i++;
             j++;
-            next[i]=1;//也就是1.即对应第一种情况：求next[0]
+            next[i]=j;//对应j找到0第一个元素仍然没有找到对应的字符
         }
-        else
+        else 
         if(sample[i]==sample[j]){
-            ++i;
-            ++j;
-            next[i]=j;//j+1，对应了pk=pj,那么next[j+1]=next[j]+1
+            i++;
+            j++;
+            next[i]=j;//j+1，对应了pk=pj,那么next[j+1]=next[j]+1 
         }
         else j=next[j];//对应pk!=pj,那么就一层层地向下去走，知道找到pj与某个匹配，或不存在匹配
     }
